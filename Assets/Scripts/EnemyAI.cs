d2aebb72471df436e0de9eced60fa24afa33e53b
captaindarkness <jonathanstrandbech@gmail.com>
2014-04-21 14:34:56 +0200

    Simple Enemy AI
    -Added Capsule
    -Added Blue color material
    -Added EnemyAI script (Follows the player anywhere in the world as of
    now)
    -Added EnemyAI script to Capsule (works)

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+	public Transform target;
+	public Transform myTransform;
+	public float moveSpeed = 0.5f;
+	public float rotationSpeed = 3.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindWithTag ("Player").transform;
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
+
+		myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+	}
+}
