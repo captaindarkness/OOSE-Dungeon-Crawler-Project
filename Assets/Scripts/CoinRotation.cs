f8a2c204bfdae514344df6647a9413bf43eebe7e
Dennis <dennis2106@live.dk>
2014-04-24 12:15:34 +0200

    Coin GUI + rotating coin object
    Rotating coins can now be added to the scene.
    
    Gui added with how many coins (gold) that have been collected.

---
+++ Assets/Scripts/CoinRotation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class CoinRotation : MonoBehaviour
+{
+	private void Update()
+	{
+		transform.Rotate(Vector3.forward * Time.deltaTime * 100);
+	}
+}
---
+++ Assets/Scripts/CollectCoins.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectCoins : MonoBehaviour {
+	public float coins;
+	
+	void  OnTriggerEnter (  Collider other   ){
+		if (other.tag == "coin") {
+			coins += 1;
+			Destroy(other.gameObject);
+		}
+	}
+	
+	void  OnGUI () {
+		
+		GUI.Label ( new Rect(20, 20, 200, 40), coins + "");
+		
+	}
+}
