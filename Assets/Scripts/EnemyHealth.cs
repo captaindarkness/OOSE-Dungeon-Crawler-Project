31fe010b83e40944ddfb502eb351942a71c8b466
Dennis <dennis2106@live.dk>
2014-04-26 15:29:04 +0200

    PLAYER ATTACK SYSTEM + ENEMY HEALTH
    Player can now attack with the spacebar.
    
    Enemy has health and will dissapear when health reaches 0.

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public int maxHealth = 100;
+	public int curHealth = 100;
+
+	// Update is called once per frame
+	void Update () {
+		AddjustCurrentHealth(0);
+	}
+
+	public void AddjustCurrentHealth(int addj){
+		curHealth += addj;
+
+		if(curHealth <= 0){
+			Destroy (gameObject);
+		}
+
+		if(curHealth > maxHealth){
+			curHealth = maxHealth;
+		}
+
+		if(maxHealth < 1){
+			maxHealth = 1;
+		}
+	}
+}
