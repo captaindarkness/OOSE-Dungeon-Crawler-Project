8aec7683604c6949b9c1584442815441d92f7eb6
Dennis <dennis2106@live.dk>
2014-04-27 12:52:35 +0200

    Health pick-ups
    Added health pick-ups for the player.
    
    Added fire for future use.

---
+++ Assets/Scripts/CollectHealth.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectHealth : MonoBehaviour {
+
+	public Health health;
+
+	void  OnTriggerEnter (  Collider other   ){
+		if (other.gameObject.name == "healthup") {
+			health.modifyHealth(+5);
+			Destroy(other.gameObject);
+		}
+	}
+}
